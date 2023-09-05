87ec12cc45547063ae7309d04d2268166bfcbb58
Sean Wilson <sean.wilson@mrjaffesclass.com>
2023-09-05 11:25:39 -0700

    HI

--- HelloWorld/HelloWorld.java
+++ HelloWorld/HelloWorld.java
@@ -2,6 +2,10 @@ public class HelloWorld
 {
     public static void main(String[] args) 
     {
+        int i;
+        double f=2.5;
+        boolean flag=true;
         System.out.println("Hello World!");
+        System.out.println(f);
     }
 }
